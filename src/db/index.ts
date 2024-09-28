20de230dd66ca629dca83a2a3d0abac72cc57897
amichaeel <mail@anthonymham.com>
2024-09-28 00:14:24 -0400

    remove old index for db

--- src/db/index.ts
+++
@@ -1,7 +0,0 @@
-import { sql } from '@vercel/postgres';
-import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { config } from 'dotenv';
-
-config({ path: '.env' });
-
-export const db = drizzle(sql);
