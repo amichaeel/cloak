18f42ca8a13a6f3ba55c84807a889bdd52790bbd
Edwin <Flattoedwin@gmail.com>
2024-09-28 13:28:02 -0400

    fix errors with posts

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { InferModel, relations, sql } from "drizzle-orm";
+import {  relations, sql } from "drizzle-orm";
 import {
   integer,
   serial,
@@ -7,8 +7,12 @@ import {
   jsonb,
   pgTableCreator,
   pgTable,
-  uuid
+  uuid,
+  index,
+  primaryKey,
+  text
 } from "drizzle-orm/pg-core";
+import { type AdapterAccount } from "next-auth/adapters";
 
 export const createTable = pgTableCreator((name) => name);  
 
@@ -23,7 +27,34 @@ export const users = pgTable('user', {
   password: varchar('password', { length: 255 }).notNull(),
   createdAt: timestamp('created_at', { withTimezone: true }).default(sql`CURRENT_TIMESTAMP`).notNull(),
 });
-
+export const accounts = createTable(
+  "account",
+  {
+    userId: varchar("user_id", { length: 255 })
+      .notNull()
+      .references(() => users.id),
+    type: varchar("type", { length: 255 })
+      .$type<AdapterAccount["type"]>()
+      .notNull(),
+    provider: varchar("provider", { length: 255 }).notNull(),
+    providerAccountId: varchar("provider_account_id", {
+      length: 255,
+    }).notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: varchar("token_type", { length: 255 }),
+    scope: varchar("scope", { length: 255 }),
+    id_token: text("id_token"),
+    session_state: varchar("session_state", { length: 255 }),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+    userIdIdx: index("account_user_id_idx").on(account.userId),
+  })
+);
 // Forms Table
 export const forms = createTable('form', {
   id: serial('id').primaryKey(),
